c66a54e116fcf70097c1ea05a95ef7a0aff3c37d
Kevin de Souza Pereira <kevinnsouza248@gmail.com>
2026-01-16 18:33:11 -0300

    Removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,6 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/home.html', context={'name': 'Kevin',})
 
-def contato(request):
-    return render(request, 'me-apague/temp.html')
 
-def sobre(request):
-    return HttpResponse('SOBRE')
